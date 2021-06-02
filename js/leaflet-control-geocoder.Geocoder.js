7338e5924d1074f33975c3186a9a92164c4519d2
Soniartha Simarmata <soniarthasimarmata@gmail.com>
2021-06-02 15:16:29 +0700

    mengganti search lokasi

--- js/leaflet-control-geocoder.Geocoder.js
+++ js/leaflet-control-geocoder.Geocoder.js
@@ -10,8 +10,8 @@ module.exports = {
 			collapsed: true,
 			expand: 'touch', // options: touch, click, anythingelse
 			position: 'topright',
-			placeholder: 'Search...',
-			errorMessage: 'Nothing found.',
+			placeholder: 'Cari lokasi',
+			errorMessage: 'Tidak ditemukan.',
 			suggestMinLength: 3,
 			suggestTimeout: 250,
 			defaultMarkGeocode: true
